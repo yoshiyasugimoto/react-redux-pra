7693ec840494e4c8feb84e433205a5ff43990678
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-11-28 13:51:41 +0900

    create action

--- react-redux-pra/src/App.js
+++ react-redux-pra/src/App.js
@@ -1,11 +1,32 @@
-function App() {
-  return (
-    <div className="App">
-        <p>
-          Hello World
-        </p>
-    </div>
-  );
-}
+import React, {Component} from 'react'
 
+
+
+const App = () => <div><Counter></Counter></div>
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = {count:0}
+  }
+  handlePlusButton = () => {
+    this.setState({count : this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({count : this.state.count - 1})
+  }
+
+  render() {
+    console.log("render")
+    return (
+      <>
+      <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </>
+    )
+  }
+
+}
 export default App;
